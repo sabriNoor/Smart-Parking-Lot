2c4562ad55280496a3cd33610f2fb327e684616e
sabriNoor <nooraosama63@gmail.com>
2025-07-28 10:02:33 +0300

    feat(domain): add VehicleType enum to represent supported vehicle types

---
+++ SmartParkingLot/Domain/Enums/VehicleType.cs
@@ -0,0 +1,15 @@
+namespace SmartParkingLot.Domain.Enums
+{
+    /// <summary>
+    /// Represents different types of vehicles that can park in the parking lot.
+    /// </summary>     
+    public enum VehicleType
+    {
+        Unknown = 0,
+        Car = 1,
+        Motorcycle = 2,
+        Truck = 3,
+        Bus = 4,
+        Bicycle = 5
+    }
+}
