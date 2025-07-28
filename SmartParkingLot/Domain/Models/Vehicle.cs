d0f73cb4855b82fcdad08b8f1d6c5151039dbc53
sabriNoor <nooraosama63@gmail.com>
2025-07-28 10:44:12 +0300

    feat(domain): add abstract Vehicle class as base for all vehicle types

---
+++ SmartParkingLot/Domain/Models/Vehicle.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using SmartParkingLot.Domain.Enums;
+
+namespace SmartParkingLot.Domain.Models
+{
+    abstract class Vehicle
+    {
+        public required string LicensePlate { get; set; }
+        public DateTime EntryTime { get; set; }
+        /// <summary>
+        /// Represents the type of vehicle.
+        /// This property is abstract and must be implemented by derived classes.
+        /// No setter is provided to ensure that the type is defined by the specific vehicle class.
+        /// </summary>
+        public abstract VehicleType Type { get; }
+
+        public Vehicle()
+        {
+            EntryTime = DateTime.Now;
+        }
+
+        public Vehicle(string licensePlate)
+        {
+            LicensePlate = licensePlate;
+            EntryTime = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type} - {LicensePlate} - Entry Time: {EntryTime}";
+        }
+             
+    }
+}
