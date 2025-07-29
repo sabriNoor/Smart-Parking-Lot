1e4fe88cf228258ac97d2078f6aae77a1f4e147c
sabriNoor <nooraosama63@gmail.com>
2025-07-29 14:48:49 +0300

    feat: configure Serilog logger and initialize core services, views, and main menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Logging;
+using SmartParkingLot.Presentation.Interfaces;
+using SmartParkingLot.Presentation.Views;
+using Serilog;
+using SmartParkingLot.Core.Services.Interfaces;
+using SmartParkingLot.Core.Services;
+using SmartParkingLot.Domain.Interfaces;
+using SmartParkingLot.Presentation.Menus;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using ILoggerFactory loggerFactory = CreateLoggerFactory();
+
+        var logger = loggerFactory.CreateLogger<Program>();
+
+        logger.LogInformation("Application started.");
+
+        IRateProvider rateProvider = new RateProvider(loggerFactory.CreateLogger<RateProvider>());
+        IFeeCalculator feeCalculator = new FeeCalculator(rateProvider);
+        IParkingLotManager parkingLotManager = new ParkingLotManager(10, loggerFactory.CreateLogger<ParkingLotManager>(), feeCalculator);
+        IMainMenuView mainMenuView = new MainMenuView(parkingLotManager, loggerFactory.CreateLogger<MainMenuView>());
+        IVehicleQueryMenu vehicleQueryMenu = new VehicleQueryMenu(new VehicleQueryView(parkingLotManager, loggerFactory.CreateLogger<VehicleQueryView>()), loggerFactory.CreateLogger<VehicleQueryMenu>());
+        IMainMenu mainMenu = new MainMenu(mainMenuView, vehicleQueryMenu, loggerFactory.CreateLogger<MainMenu>());
+        INotifiable consoleNotification = new ConsoleNotification(loggerFactory.CreateLogger<ConsoleNotification>());
+        mainMenu.RenderMainMenu();
+
+    }
+
+    private static ILoggerFactory CreateLoggerFactory()
+    {
+        try
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/smart_parking_lot.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.ClearProviders();
+            builder.AddSerilog();
+        });
+            return loggerFactory;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error initializing Serilog: {ex.Message}");
+            throw;
+        }
+    }
+}
